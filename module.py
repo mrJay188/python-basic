12413cc9ef64d78121271fd1fc8c23d0f52967ba
mrJay188 <163376106+mrJay188@users.noreply.github.com>
2026-03-13 10:56:46 +0530

    commit

---
+++ module.py
@@ -0,0 +1,16 @@
+# create module and import and use dunction in different ways.
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    if b == 0:
+        return "Divition by zero not right method"
+    return a/b
+
